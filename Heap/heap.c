cb31b62a13524f35baf178a86c04aec687a7cf04
DaviDarkFire <daviferreirasantog@gmail.com>
2016-01-20 12:18:07 -0300

    heap boladão

---
+++ Heap/heap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define N 10
+
+int pai(int i){
+
+	return (i-1)/2;
+
+}
+
+int filho_esquerda(int i){
+
+
+	return 2*i+1;
+
+
+}
+
+int filho_direita(int i){
+
+	return 2*+2;
+
+
+}
+
+int insere(int s[N], int *n){
+	 
+}
+
+int main(void){
+	int s[N] = {26, 18, 14, 16, 8, 9, 11, 4, 12, 6};
+
+	return 0;
+}
